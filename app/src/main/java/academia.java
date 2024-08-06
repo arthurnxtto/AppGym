a3a7b53251fef782959a58237f064b80f110625c
arthurnxtto <arthurvnetto8@outlook.com>
2024-08-06 20:28:13 -0300

    Incluindo atualizações

---
+++ app/src/main/java/academia.java
@@ -0,0 +1,2 @@
+public class academia {
+}
---
+++ app/src/main/java/br/com/etecia/myapp/academia.java
@@ -0,0 +1,43 @@
+package br.com.etecia.myapp;
+
+public class academia {
+    private String titulo;
+    private String descricao;
+    private String note;
+    private int imgAcademia;
+
+    public academia() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getImgAcademia() {
+        return imgAcademia;
+    }
+
+    public void setImgAcademia(int imgAcademia) {
+        this.imgAcademia = imgAcademia;
+    }
+}
